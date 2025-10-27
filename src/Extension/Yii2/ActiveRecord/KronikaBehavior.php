22eceebdbe48998e17a561bac2a85f6dc0caa0d4
frostealth <frostealth@gmail.com>
2025-10-27 20:22:07 +0600

    add timezone storing and forcing options for yii2

--- src/Extension/Yii2/ActiveRecord/KronikaBehavior.php
+++ src/Extension/Yii2/ActiveRecord/KronikaBehavior.php
@@ -25,7 +25,7 @@ use yii\db\BaseActiveRecord;
 /**
  * KronikaBehavior automatically serializes/deserializes Kronika objects into/from Database types.
  *
- * ```php
+ * ```
  * use Kronika\Time;
  * use Kronika\ZonedDateTime;
  * use Kronika\Extension\Yii2\ActiveRecord\KronikaBehavior;
@@ -47,12 +47,19 @@ use yii\db\BaseActiveRecord;
  *                     // class-string      => attribute names
  *                     Time::class          => ['opening', 'closing'],
  *                     ZonedDateTime::class => ['createdAt', 'updatedAt'],
+ *                     \DateTimeZone::class => ['timezone'],
+ *                 ],
+ *                 'timezone' => [
+ *                     'force' => fn():\DateTimeZone => $this->timezone,
  *                 ],
  *             ],
  *         ];
  *     }
  * }
+ * ```
  *
+ * @example
+ * ```
  * use function Kronika\now;
  *
  * $foo = new Foo();
@@ -83,15 +90,19 @@ use yii\db\BaseActiveRecord;
  * - Instant         -> float
  * - LocalDateTime   -> string (datetime without time-zone)
  * - ZonedDateTime   -> string (datetime with time-zone)
+ * - \DateTimeZone   -> string
  *
  * @psalm-type TAttributeName=non-empty-string
  * @psalm-type TFormattable=Date|Time|LocalDateTime|ZonedDateTime
+ * @psalm-type TFormatOptions=array{class-string<TFormattable>, non-empty-string}
+ * @psalm-type TForcedTimezone=null|\DateTimeZone|callable(non-empty-string=): ?\DateTimeZone
+ * @psalm-type TTimezoneOptions=array{store: bool, suffix: non-empty-string, force: TForcedTimezone}
  *
  * @property BaseActiveRecord $owner
  */
 final class KronikaBehavior extends Behavior
 {
-    /** @var array<class-string<TFormattable>, non-empty-string> */
+    /** @var TFormatOptions */
     public static array $defaultFormats = [
         Date::class => 'Y-m-d',
         Time::class => 'H:i:s',
@@ -99,8 +110,15 @@ final class KronikaBehavior extends Behavior
         ZonedDateTime::class => \DateTimeInterface::ATOM,
     ];
 
+    /** @var TTimezoneOptions */
+    public static array $defaultTimezone = [
+        'store' => false,
+        'suffix' => '_Timezone',
+        'force' => null,
+    ];
+
     /**
-     * ```php
+     * ```
      * 'attributes' => [
      *     Date::class => ['registeredOn'],
      *     Time::class => ['opening', 'closing'],
@@ -113,15 +131,84 @@ final class KronikaBehavior extends Behavior
     public array $attributes = [];
 
     /**
-     * ```php
+     * ```
      * 'formats' => [
      *     Time::class => 'H:i:s.u',
      * ],
      * ```
      *
-     * @var array<class-string<TFormattable>, non-empty-string>
+     * @var TFormatOptions
      */
     public array $formats = [];
+
+    /**
+     * ```
+     * // storing time-zones in an additional attributes
+     * // with the 'Timezone' suffix ('createdAtTimezone').
+     * 'attributes' => [
+     *     ZonedDateTime::class => ['createdAt', 'updateAt'],
+     * ],
+     * 'timezone' => [
+     *     'store' => true,  // default false
+     *     'suffix' => 'Timezone',
+     * ],
+     *
+     * $foo = new Foo();
+     * $foo->createdAt = now(new \DateTimeZone("UTC"));
+     * $foo->updatedAt = now(new \DateTimeZone("+02:00"));
+     * $foo->save(); $foo->refresh();
+     *
+     * $foo->createdAt->format("Y-m-d H:i:s P"); // 2025-12-31 12:15:45 +00:00
+     * $foo->updatedAt->format("Y-m-d H:i:s P"); // 2025-12-31 14:15:45 +02:00
+     * $foo->createdAtTimezone->getName();       // UTC
+     * $foo->updatedAtTimezone->getName();       // +02:00
+     *
+     * // forcing a time-zone.
+     * // all ZonedDateTime will be shifted to a given time-zone.
+     * 'attributes' => [
+     *     ZonedDateTime::class => ['createdAt', 'updatedAt'],
+     *     \DateTimeZone::class => ['timezone'],
+     * ],
+     * 'timezone' => [
+     *     'force' => fn():\DateTimeZone => $this->timezone,  // default null
+     * ],
+     *
+     * $foo = new Foo();
+     * $foo->createdAt = now(new \DateTimeZone("UTC"));
+     * $foo->updatedAt = now(new \DateTimeZone("+02:00"));
+     * $foo->timezone = new \DateTimeZone("+01:00");
+     * $foo->save(); $foo->refresh();
+     *
+     * $foo->createdAt->format("Y-m-d H:i:s P"); // 2025-12-31 13:15:45 +01:00
+     * $foo->updatedAt->format("Y-m-d H:i:s P"); // 2025-12-31 13:15:45 +01:00
+     * ```
+     *
+     * @var TTimezoneOptions
+     */
+    public array $timezone = [];
+
+    /**
+     * ```
+     * 'attributes' => [
+     *     ZonedDateTime::class => ['createdAt'],
+     * ],
+     * 'timezone' => [
+     *     'force' => \DateTimeZone('UTC'),
+     * ],
+     *
+     * $foo = new Foo();
+     * $foo->createdAt = now(new \DateTimeZone("+01:00"));
+     * $foo->createdAt->format("Y-m-d\TH:i:sP"); // 2025-12-31T13:15:45+01:00
+     *
+     * $foo->syncTimezones();
+     * $foo->createdAt->format("Y-m-d\TH:i:sP"); // 2025-12-31T12:15:45+00:00
+     * ```
+     */
+    public function syncTimezones(): void
+    {
+        $this->forceTimezone();
+        $this->storeTimezones();
+    }
 
     #[\Override]
     public function attach($owner): void
@@ -136,25 +223,26 @@ final class KronikaBehavior extends Behavior
     public function events(): array
     {
         return [
-            BaseActiveRecord::EVENT_AFTER_FIND => $this->castAttributesToObjects(...),
-            BaseActiveRecord::EVENT_AFTER_INSERT => $this->castAttributesToObjects(...),
-            BaseActiveRecord::EVENT_AFTER_UPDATE => $this->castAttributesToObjects(...),
+            BaseActiveRecord::EVENT_AFTER_FIND => $this->castAllToObjects(...),
+            BaseActiveRecord::EVENT_AFTER_INSERT => $this->castAllToObjects(...),
+            BaseActiveRecord::EVENT_AFTER_UPDATE => $this->castAllToObjects(...),
 
-            BaseActiveRecord::EVENT_BEFORE_INSERT => $this->castAttributesToDatabaseValues(...),
-            BaseActiveRecord::EVENT_BEFORE_UPDATE => $this->castAttributesToDatabaseValues(...),
+            BaseActiveRecord::EVENT_BEFORE_INSERT => $this->castAllToDatabaseValues(...),
+            BaseActiveRecord::EVENT_BEFORE_UPDATE => $this->castAllToDatabaseValues(...),
         ];
     }
 
     private function assertAttributes(BaseActiveRecord $owner): void
     {
-        static $kronikaTypes = [
+        static $supportedTypes = [
             Date::class, Date\Year::class, Date\Month::class, Date\DayOfMonth::class, Date\DayOfWeek::class,
             Time::class, Time\Hour::class, Time\Minute::class, Time\Second::class,
             Duration::class, Instant::class, LocalDateTime::class, ZonedDateTime::class,
+            \DateTimeZone::class,
         ];
 
         foreach ($this->attributes as $type => $names) {
-            \assert(\in_array($type, $kronikaTypes, true), "Unknown type: [$type]");
+            \assert(\in_array($type, $supportedTypes, true), "Unknown type: [$type]");
             foreach ($names as $name) {
                 \assert($owner->hasAttribute($name), \sprintf(
                     "Attribute [%s::%s] doesn't exist",
@@ -165,9 +253,24 @@ final class KronikaBehavior extends Behavior
         }
     }
 
-    private function castAttributesToDatabaseValues(): void
+    private function castAllToDatabaseValues(): void
     {
-        foreach ($this->attributeNames() as $attributeName) {
+        $this->syncTimezones();
+
+        $this->castAttributesToDatabaseValues($this->attributeNames());
+        $this->castAttributesToDatabaseValues($this->timezoneAttributesNames());
+    }
+
+    private function castAllToObjects(): void
+    {
+        $this->castAttributesToObjects($this->timezoneAttributesNames());
+        $this->castAttributesToObjects($this->attributeNames());
+    }
+
+    /** @param list<TAttributeName> $attributes */
+    private function castAttributesToDatabaseValues(array $attributes): void
+    {
+        foreach ($attributes as $attributeName) {
             $attribute = $this->owner->getAttribute($attributeName);
             if (\is_object($attribute)) {
                 $this->owner->setAttribute($attributeName, $this->toDatabaseValue($attribute));
@@ -180,9 +283,10 @@ final class KronikaBehavior extends Behavior
         }
     }
 
-    private function castAttributesToObjects(): void
+    /** @param list<TAttributeName> $attributes */
+    private function castAttributesToObjects(array $attributes): void
     {
-        foreach ($this->attributeNames() as $attributeName) {
+        foreach ($attributes as $attributeName) {
             $attribute = $this->owner->getAttribute($attributeName);
             if (null !== $attribute) {
                 $this->owner->setAttribute($attributeName, $this->toObject($attributeName, $attribute));
@@ -211,37 +315,89 @@ final class KronikaBehavior extends Behavior
             $obj instanceof Instant => $obj->value(),
             $obj instanceof LocalDateTime => $obj->format($this->getFormatFor(LocalDateTime::class)),
             $obj instanceof ZonedDateTime => $obj->format($this->getFormatFor(ZonedDateTime::class)),
+            $obj instanceof \DateTimeZone => $obj->getName(),
             default => throw new \RuntimeException(\sprintf('Unknown type: [%s]', \get_debug_type($obj))),
         };
     }
 
+    /**
+     * @param TAttributeName $attributeName
+     * @param numeric|non-empty-string $value
+     */
     private function toObject(string $attributeName, float|int|string $value): object
     {
         return match ($this->getTypeOf($attributeName)) {
-            Date::class            => Date::ofFormat(format: $this->getFormatFor(Date::class), date: (string) $value),
-            Date\Year::class       => Date\Year::of((int) $value),
-            Date\Month::class      => Date\Month::of((int) $value),
-            Date\DayOfMonth::class => Date\DayOfMonth::of((int) $value),
-            Date\DayOfWeek::class  => Date\DayOfWeek::of((int) $value),
-            Time::class            => Time::ofFormat(format: $this->getFormatFor(Time::class), time: (string) $value),
-            Time\Hour::class       => Time\Hour::of((int) $value),
-            Time\Minute::class     => Time\Minute::of((int) $value),
-            Time\Second::class     => Time\Second::of(...\sscanf((string) $value, '%d.%6d')),
-            Duration::class        => Duration::of(seconds: (int) $value),
+            Date::class            => Date::ofFormat(format: $this->getFormatFor(Date::class), date: (string)$value),
+            Date\Year::class       => Date\Year::of((int)$value),
+            Date\Month::class      => Date\Month::of((int)$value),
+            Date\DayOfMonth::class => Date\DayOfMonth::of((int)$value),
+            Date\DayOfWeek::class  => Date\DayOfWeek::of((int)$value),
+            Time::class            => Time::ofFormat(format: $this->getFormatFor(Time::class), time: (string)$value),
+            Time\Hour::class       => Time\Hour::of((int)$value),
+            Time\Minute::class     => Time\Minute::of((int)$value),
+            Time\Second::class     => Time\Second::of(...\sscanf((string)$value, '%d.%6d')),
+            Duration::class        => Duration::of(seconds: (int)$value),
             Instant::class         => Instant::ofValue($value),
             LocalDateTime::class   => ZonedDateTime::ofFormat(
-                format: $this->getFormatFor(ZonedDateTime::class),
-                datetime: (string) $value,
+                format: $this->getFormatFor(LocalDateTime::class),
+                datetime: (string)$value,
             )->toLocalDateTime(),
-            ZonedDateTime::class   => ZonedDateTime::ofFormat(
+            ZonedDateTime::class   => $this->adjustTimezone($attributeName, ZonedDateTime::ofFormat(
                 format: $this->getFormatFor(ZonedDateTime::class),
-                datetime: (string) $value,
-            ),
+                datetime: (string)$value,
+            )),
+            \DateTimeZone::class   => new \DateTimeZone((string)$value),
         };
     }
 
+    private function forceTimezone(): void
+    {
+        $timezone = $this->forcedTimezone();
+        if (! $timezone instanceof \DateTimeZone) {
+            return;
+        }
+
+        foreach ($this->zonedAttributeNames() as $name) {
+            $datetime = $this->owner->getAttribute($name);
+            $oldDatetime = $this->owner->getOldAttribute($name);
+            if (! $datetime instanceof ZonedDateTime) {
+                continue;
+            }
+
+            $this->owner->setAttribute($name, $datetime->shiftTimezone($timezone));
+            if ($this->owner->canSetOldAttribute($name)) {
+                $this->owner->setOldAttribute($name, $oldDatetime);
+            }
+        }
+    }
+
+    private function storeTimezones(): void
+    {
+        if (! $this->shouldStoreTimezone()) {
+            return;
+        }
+
+        foreach ($this->zonedAttributeNames() as $name) {
+            $datetime = $this->owner->getAttribute($name);
+            $timezone = $datetime instanceof ZonedDateTime ? $datetime->getTimezone() : null;
+
+            $this->owner->setAttribute($this->timezoneAttributeNameFor($name), $timezone);
+        }
+    }
+
+    /** @param TAttributeName $name */
+    private function adjustTimezone(string $name, ZonedDateTime $datetime): ZonedDateTime
+    {
+        $timezone = $this->forcedTimezone() ?? $this->owner->getAttribute($this->timezoneAttributeNameFor($name));
+        if (! $timezone instanceof \DateTimeZone) {
+            $timezone = $datetime->timezone();
+        }
+
+        return $datetime->shiftTimezone($timezone);
+    }
+
     /**
-     * @param class-string<Date|Time|LocalDateTime|ZonedDateTime> $type
+     * @param class-string<TFormattable> $type
      *
      * @return non-empty-string
      */
@@ -251,7 +407,7 @@ final class KronikaBehavior extends Behavior
     }
 
     /**
-     * @param non-empty-string $attributeName
+     * @param TAttributeName $attributeName
      *
      * @return class-string
      */
@@ -263,6 +419,10 @@ final class KronikaBehavior extends Behavior
             }
         }
 
+        if (\str_ends_with($attributeName, $this->timezoneSuffix())) {
+            return \DateTimeZone::class;
+        }
+
         throw new \RuntimeException("Unknown attribute: [$attributeName]");
     }
 
@@ -272,5 +432,51 @@ final class KronikaBehavior extends Behavior
         foreach ($this->attributes as $names) {
             yield from $names;
         }
+    }
+
+    /** @return iterable<TAttributeName> */
+    private function zonedAttributeNames(): iterable
+    {
+        yield from $this->attributes[ZonedDateTime::class] ?? [];
+    }
+
+    /** @return iterable<TAttributeName> */
+    private function timezoneAttributesNames(): iterable
+    {
+        if (! $this->shouldStoreTimezone()) {
+            return [];
+        }
+
+        foreach ($this->zonedAttributeNames() as $attributeName) {
+            yield $this->timezoneAttributeNameFor($attributeName);
+        }
+    }
+
+    /**
+     * @param TAttributeName $attributeName
+     *
+     * @return TAttributeName
+     */
+    private function timezoneAttributeNameFor(string $attributeName): string
+    {
+        return $attributeName . $this->timezoneSuffix();
+    }
+
+    /** @return non-empty-string */
+    private function timezoneSuffix(): string
+    {
+        return $this->timezone['suffix'] ?? self::$defaultTimezone['suffix'] ?? '_Timezone';
+    }
+
+    private function forcedTimezone(): ?\DateTimeZone
+    {
+        $timezone = $this->timezone['force'] ?? self::$defaultTimezone['force'] ?? null;
+
+        return \is_callable($timezone) ? $timezone() : $timezone;
+    }
+
+    private function shouldStoreTimezone(): bool
+    {
+        return $this->timezone['store'] ?? self::$defaultTimezone['store'] ?? false;
     }
 }
