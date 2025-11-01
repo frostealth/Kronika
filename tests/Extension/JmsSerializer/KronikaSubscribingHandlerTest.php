195dc5e45a8ea3eb6116de788914f3a19cd72400
frostealth <frostealth@gmail.com>
2025-11-02 03:50:12 +0600

    add duration format options support for jms serializer extension

--- tests/Extension/JmsSerializer/KronikaSubscribingHandlerTest.php
+++ tests/Extension/JmsSerializer/KronikaSubscribingHandlerTest.php
@@ -87,6 +87,8 @@ final class KronikaSubscribingHandlerTest extends TestCase
             second: Time\Second::of(45, 6789),
             secondAlias: Time\Second::of(40, 5555),
             duration: Duration::of(days: 1, hours: 2, minutes: 25, seconds: 99),
+            durationInHours: Duration::of(days: 1, hours: 2, minutes: 25, seconds: 99),
+            durationArray: Duration::of(days: 1, hours: 2, minutes: 25, seconds: 99),
             durationAlias: Duration::of(days: 1, hours: 1, minutes: 25, seconds: 80),
             instant: Instant::of(123456789, 54321),
             instantAlias: Instant::of(123456780, 54310),
@@ -140,7 +142,17 @@ final class KronikaSubscribingHandlerTest extends TestCase
         self::assertSame($this->entry->secondAlias, $unserialized->secondAlias);
 
         self::assertSame($this->entry->duration, $unserialized->duration);
+        self::assertEquals($this->entry->durationInHours->roundToHours(), $unserialized->durationInHours);
+        self::assertEquals($this->entry->durationInHours->inHours(), $serialized['durationInHours']);
+        self::assertSame($this->entry->durationArray, $unserialized->durationArray);
+        self::assertEquals([
+            'days' => $this->entry->durationArray->days(),
+            'hours' => $this->entry->durationArray->hours(),
+            'minutes' => $this->entry->durationArray->minutes(),
+            'seconds' => $this->entry->durationArray->seconds(),
+        ], $serialized['durationArray']);
         self::assertSame($this->entry->durationAlias, $unserialized->durationAlias);
+
         self::assertSame($this->entry->instant, $unserialized->instant);
         self::assertSame($this->entry->instantAlias, $unserialized->instantAlias);
 
